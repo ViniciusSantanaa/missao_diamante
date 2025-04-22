bb2708c41071b391165c12ce906c5f4fbc849ecd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-22 20:48:28 -0300

    commit teste

---
+++ src/main/java/br/com/fiap/missao_diamante_api/MissaoDiamanteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.missao_diamante_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissaoDiamanteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissaoDiamanteApiApplication.class, args);
+	}
+
+}
